441537c5c2ea9733bad7512cdefa14ee05a2488a
Brad Erickson <bderickson@gmail.com>
2019-02-21 18:24:35 -0800

    fix: cloudflare-worker-mock explicitly depends on both other packages for types

--- packages/@udacity/cloudflare-worker-mock/index.ts
+++ packages/@udacity/cloudflare-worker-mock/index.ts
@@ -1,6 +1,11 @@
 // Primarily exists to implement the Cache API:
 // https://developers.cloudflare.com/workers/reference/cache-api/
 
+// Import the service-worker-mock types.
+/// <reference types="@udacity/types-service-worker-mock" />
+// Import the service-worker-mock global augmentations.
+import '@udacity/types-service-worker-mock/global';
+
 import { CloudFlareCacheQueryOptions, CloudflareWorkerGlobalScopePatch } from '@udacity/types-cloudflare-worker';
 import makeServiceWorkerEnv from 'service-worker-mock';
 import { EnvOptions } from 'service-worker-mock';
