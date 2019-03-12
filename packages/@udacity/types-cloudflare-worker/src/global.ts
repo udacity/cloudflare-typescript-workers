10eb2e913876d9ba0fec3fe8d1fedc14b221c076
Brad Erickson <bderickson@gmail.com>
2019-03-11 17:09:42 -0700

    chore: Add warning about missing Request Attributes in Preview

--- packages/@udacity/types-cloudflare-worker/src/global.ts
+++ packages/@udacity/types-cloudflare-worker/src/global.ts
@@ -77,19 +77,25 @@ export interface CloudflareWorkerGlobalScopePatch {
  * CloudFlare Request Attributes
  *
  * Workers allows you to run custom logic based for any incoming request. In
- * addition to the information available on the Request object, such as
- * headers, Cloudflare provides additional attributes of the request using
- * the request.cf object.
+ * addition to the information available on the Request object, such as headers,
+ * Cloudflare provides additional attributes of the request using the request.cf
+ * object.
  *
  * Attributes available on request.cf:
  * * tlsVersion: the TLS version used on the connection to Cloudflare.
  * * tlsCipher: the cipher used on the connection to Cloudflare.
- * * country: the two letter country code on the request (this is the same
- *   value as the one provided by the CF-IPCountry header)
+ * * country: the two letter country code on the request (this is the same value
+ *   as the one provided by the CF-IPCountry header)
  * * colo: the three letter airport code of the colo the request hit.
  *
  * Attributes available through headers:
  * * Client IP: the client IP is available via the CF-Connecting-IP header.
+ *
+ * WARNING: Request Attributes do not currently work in the Worker Editor
+ * Preview, resulting in an error: "Uncaught (in response) TypeError: Cannot
+ * read property 'country' of undefined." See:
+ * https://developers.cloudflare.com/workers/recipes/tls-version-blocking/
+ *
  */
 export interface CloudFlareRequestAttributes extends CloudFlareRequestFeatures {
   /**
